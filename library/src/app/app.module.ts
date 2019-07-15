12ea863e43b96a638105661e76fd12c3a7f15120
huleile <hule_sky@163.com>
2019-07-15 20:52:32 +0800

    feat(auth): add authInterceptor

--- library/src/app/app.module.ts
+++ library/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { LocalStorage } from './services/storage.service';
 import { BookService } from './services/book.service';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { BooksComponent } from './books/books.component';
 import { RMBPipe } from './pipes/price.cn.pipe';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
    declarations: [
@@ -34,7 +35,12 @@ import { RMBPipe } from './pipes/price.cn.pipe';
    providers: [
       AuthService,
       BookService,
-      LocalStorage
+      LocalStorage,
+      {
+         provide: HTTP_INTERCEPTORS,
+         useClass: AuthInterceptor,
+         multi: true
+      }
    ],
    bootstrap: [
       AppComponent
--- library/src/app/interceptors/auth.interceptor.ts
+++ library/src/app/interceptors/auth.interceptor.ts
@@ -1,14 +1,28 @@
+import { tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { LocalStorage } from '../services';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+    constructor(
+        private localStorage: LocalStorage,
+        private router: Router
+    ) {}
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const clonedRequest = req.clone({
             headers: req.headers.set('token', 'happy')
         });
         console.log('new headers:', clonedRequest.headers.keys());
-        return next.handle(clonedRequest);
+        return next.handle(clonedRequest).pipe(tap(event => {
+            if (event instanceof HttpResponse) {
+                if (event.body.code === 401) {
+                    this.router.navigateByUrl('/login');
+                }
+            }
+        }));
     }
 }
